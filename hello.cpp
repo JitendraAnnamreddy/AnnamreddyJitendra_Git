cc99ba254d54d643a6c6eb63396cbc92bede722d
JitendraAnnamreddy <54024959+JitendraAnnamreddy@users.noreply.github.com>
2020-07-21 11:20:31 +0530

    CPP File

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main() 
+{ 
+    vector<int> vect; 
+  vect.push_back(100);
+    for (int x : vect) 
+        cout << x << " "; 
+  
+    return 0; 
+} 
